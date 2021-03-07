d4e5550169e12850e9d30eafde2f122d06e42c84
alexandr <zbc@mail.ru>
2021-03-07 23:53:15 +0300

    Вычислил крайние точки, с отличающимся цветом

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from PIL import Image, ImageFilter, ImageDraw
+import random
+import os
+
+
+def xline(draw, y, width):
+    for x in range(width):
+        draw.point((x, y), (0, 0, 0))
+
 
 image = Image.open('./test.png')
 pix = image.load()
@@ -10,19 +18,39 @@ draw = ImageDraw.Draw(image)
 width = image.size[0]  # Определяем ширину
 height = image.size[1]  # Определяем высоту
 
+up, bottom, left, right = 0, 0, 0, 0
+color = [pix[1, 1][0], pix[1, 1][1], pix[1, 1][2]]
+print(f'{color=}')
+
+for y in range(height):
+    if not up:
+        for x in range(width):
+            # print('2', x, y, [pix[x, y][0], pix[x, y][1], pix[x, y][2]])
+            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                up = y - 1
+                break
+
+for y in reversed(range(height)):
+    if not bottom:
+        for x in range(width):
+            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                bottom = y + 1
+                break
+
 for x in range(width):
-    for y in range(height):
-        r = pix[x, y][0]  # узнаём значение красного цвета пикселя
-        g = pix[x, y][1]  # зелёного
-        b = pix[x, y][2]  # синего
-        sr = (r + g + b) // 3  # среднее значение
-        draw.point((x, y), (sr, sr, sr))  # рисуем пиксель
+    if not left:
+        for y in range(height):
+            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                left = x - 1
+                break
+for x in reversed(range(width)):
+    if not right:
+        for y in range(height):
+            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                right = x + 1
+                break
 
-
-color = pix[1, 1][0]
-for x in range(width):
-    for y in range(height):
-        if color!=pix[x, y][0]:
-            draw.point((x, y), (255, 0, 0))  # рисуем пиксель
-
+print(f'{up=}', f'{bottom=}', f'{left=}', f'{right=}', )
 image.save("result.jpg", "JPEG")
+
+# draw.point((x, y), (0, 0, random.randint(1, 255)))  # рисуем пиксель
