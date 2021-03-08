74cd89a034fc75bc0a57f8744af1f4f3f1c71a8e
Artem Davletov <Tomas-Cooper@mail.ru>
2021-03-08 16:04:53 +0300

    Wrap main.py code and add console working

--- main.py
+++ main.py
@@ -1,49 +1,64 @@
+import sys
+
 from PIL import Image, ImageFilter, ImageDraw
 
-image = Image.open('./test.png')
-pix = image.load()
 
-print(f'{image.format=}', f'{image.size=}', f'{image.mode=}')
-print('Размер изображения', image.size[0], image.size[1])
+def crop(image):
+    pix = image.load()
 
-draw = ImageDraw.Draw(image)
+    print(f'{image.format=}', f'{image.size=}', f'{image.mode=}')
+    print('Размер изображения', image.size[0], image.size[1])
 
-width = image.size[0]  # Определяем ширину
-height = image.size[1]  # Определяем высоту
+    draw = ImageDraw.Draw(image)
 
-up, bottom, left, right = 0, 0, 0, 0
-color = [pix[1, 1][0], pix[1, 1][1], pix[1, 1][2]]
-print('Цвет пикселя (1,1)', f'{color=}')
+    width = image.size[0]  # Определяем ширину
+    height = image.size[1]  # Определяем высоту
 
-for y in range(height):
-    if not up:
-        for x in range(width):
-            # print('2', x, y, [pix[x, y][0], pix[x, y][1], pix[x, y][2]])
-            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
-                up = y - 1
-                break
+    up, bottom, left, right = 0, 0, 0, 0
+    color = [pix[1, 1][0], pix[1, 1][1], pix[1, 1][2]]
+    print('Цвет пикселя (1,1)', f'{color=}')
 
-for y in reversed(range(height)):
-    if not bottom:
-        for x in range(width):
-            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
-                bottom = y + 2
-                break
+    for y in range(height):
+        if not up:
+            for x in range(width):
+                # print('2', x, y, [pix[x, y][0], pix[x, y][1], pix[x, y][2]])
+                if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                    up = y - 1
+                    break
 
-for x in range(width):
-    if not left:
-        for y in range(height):
-            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
-                left = x - 1
-                break
-for x in reversed(range(width)):
-    if not right:
-        for y in range(height):
-            if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
-                right = x + 2
-                break
+    for y in reversed(range(height)):
+        if not bottom:
+            for x in range(width):
+                if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                    bottom = y + 2
+                    break
 
-print('Координаты обрезки', f'{up=}', f'{bottom=}', f'{left=}', f'{right=}', )
-image_crop = image.crop((left, up, right, bottom))
-image_crop.save("result_crop.jpg", "JPEG")
+    for x in range(width):
+        if not left:
+            for y in range(height):
+                if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                    left = x - 1
+                    break
+    for x in reversed(range(width)):
+        if not right:
+            for y in range(height):
+                if color != [pix[x, y][0], pix[x, y][1], pix[x, y][2]]:
+                    right = x + 2
+                    break
 
+    print('Координаты обрезки', f'{up=}', f'{bottom=}', f'{left=}', f'{right=}', )
+    image_crop = image.crop((left, up, right, bottom))
+    image_crop.save("result_crop.png", "PNG")
+
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    try:
+        image = Image.open(file_path)
+    except FileNotFoundError as e:
+        print(f"No such file or directory: {5}")
+    else:
+        try:
+            crop(image=image)
+        except Exception as e:
+            print(e)
