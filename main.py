7972315f008bf4d0ebcf75bfab2a70a87a2b2801
alexandr <zbc@mail.ru>
2021-03-12 23:24:23 +0300

    #16 fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Справка программы Резак.
 Usage:
-  main.py [-h] [--sensitivity=SENS]
-          [--postfix=POSTFIX]
+  main.py [-hv] [--sensitivity=SENS]
+          [--postfix=POSTFIX] [--verbose]
           [--void=PIXEL | --color=COLOR]
           FILES...
 
@@ -15,6 +15,7 @@ Options:
   --void=PIXEL              Координаты пикселя из рамки [default: 1,1]
   --postfix=POSTFIX         Координаты пикселя из рамки [default: crop]
   --color=COLOR             Цвет рамки HEX  [default: False]
+  --verbose                 Подробный вывод
 
 Example, try:
   main.py --postfix=crop --sensitivity=2 --void=1,1 ./demo.png ./demo2.png
@@ -27,70 +28,68 @@ from pathlib import Path
 from docopt import docopt
 
 
+def get_color(pixels, x, y, sens):
+    if isinstance(pixels[x, y], int):
+        return pixels[x, y] // sens
+    else:
+        color = [pixels[x, y][0] // sens,
+                 pixels[x, y][1] // sens,
+                 pixels[x, y][2] // sens
+                 ]
+        return color
+
+
 def crop(img, new_filename, arg):
     sens = int(arg['--sensitivity'])
     pix = img.load()
     if arg['--color'] == 'False':
         x, y = arg['--void'].split(',')
         x, y = int(x), int(y)
-        color = [pix[x, y][0] // sens,
-                 pix[x, y][1] // sens,
-                 pix[x, y][2] // sens
-                 ]
+        color = get_color(pix, x, y, sens)
     else:
         color = ImageColor.getrgb(arg['--color'])
-
-    print(f'{img.format=}', f'{img.size=}', f'{img.mode=}')
-    print('Размер изображения', img.size[0], img.size[1])
+    if arg['--verbose']:
+        print(f'{img.format=}', f'{img.size=}', f'{img.mode=}')
+        print('Размер изображения', img.size[0], img.size[1])
 
     width = img.size[0]  # Определяем ширину
     height = img.size[1]  # Определяем высоту
-    print(f'Цвет пустоты {color=}')
+    if arg['--verbose']:
+        print(f'Цвет пустоты {color=}')
 
     up, bottom, left, right = -1, -1, -1, -1
     for y in range(height):
         if up < 0:
             for x in range(width):
                 # print('2', x, y, [pix[x, y][0], pix[x, y][1], pix[x, y][2]])
-                if color != [pix[x, y][0] // sens,
-                             pix[x, y][1] // sens,
-                             pix[x, y][2] // sens
-                             ]:
+                if color != get_color(pix, x, y, sens):
                     up = y - 1
                     break
 
     for y in reversed(range(height)):
         if bottom < 0:
             for x in range(width):
-                if color != [pix[x, y][0] // sens,
-                             pix[x, y][1] // sens,
-                             pix[x, y][2] // sens
-                             ]:
+                if color != get_color(pix, x, y, sens):
                     bottom = y + 1
                     break
 
     for x in range(width):
         if left < 0:
             for y in range(height):
-                if color != [pix[x, y][0] // sens,
-                             pix[x, y][1] // sens,
-                             pix[x, y][2] // sens
-                             ]:
+                if color != get_color(pix, x, y, sens):
                     left = x - 1
                     break
     for x in reversed(range(width)):
         if right < 0:
             for y in range(height):
-                if color != [pix[x, y][0] // sens,
-                             pix[x, y][1] // sens,
-                             pix[x, y][2] // sens
-                             ]:
+                if color != get_color(pix, x, y, sens):
                     right = x + 1
                     break
-
-    print('Координаты обрезки', f'{up=} {bottom=} {left=} {right=}')
+    if arg['--verbose']:
+        print('Координаты обрезки', f'{up=} {bottom=} {left=} {right=}')
     img_crop = img.crop((left, up, right, bottom))
     img_crop.save(f'{new_filename}', "PNG")
+    print(f'Готово\nРезультат сохранен сюда - {new_filename}')
 
 
 def get_new_filename(path, postfix):
@@ -99,7 +98,8 @@ def get_new_filename(path, postfix):
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
-    print(arguments)
+    if arguments['--verbose']:
+        print(arguments)
     for f_path in arguments['FILES']:
         file_path = Path(f_path)
         if not file_path.is_file():
@@ -107,6 +107,7 @@ if __name__ == "__main__":
         else:
             image = Image.open(file_path)
             new_filename = get_new_filename(file_path, arguments['--postfix'])
-            print('\n===============!!===============')
-            print(f'Пациент - {file_path}')
+            if arguments['--verbose']:
+                print('\n===============!!===============')
+                print(f'Пациент - {file_path}')
             crop(img=image, new_filename=new_filename, arg=arguments)
